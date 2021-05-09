c6f4c893f2ed62c511054a851c8d02f7c8b52594
Michalak-Bartosz <michalak.bartoszit@gmail.com>
2021-05-09 16:48:09 +0200

    Ekran startowy, przygotowanie menu
    Został utworzony ekran startowy wraz z logo i przygotowane miejsce pod menu aplikacji.

---
+++ app/src/main/java/com/lordeats/mobeats/MainMenuFragment.kt
@@ -0,0 +1,28 @@
+package com.lordeats.mobeats
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.lordeats.mobeats.databinding.FragmentMainMenuBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainMenuFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainMenuFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainMenuBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main_menu, container, false)
+        return binding.root
+    }
+
+}
